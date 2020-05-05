c562247f43a58813b4c820ae4eccb502cf97dcac
Sergey Denisenko <sergey.denisenko@five9.com>
2020-05-05 23:53:24 +0300

    for testing purpose without testing. small refactoring

--- Games/Ticktak/views.py
+++ Games/Ticktak/views.py
@@ -1,9 +1,7 @@
 from django.http import response
-from django.shortcuts import render
+
 
 # Create your views here.
 
 def home(request):
-
-    #return response.HttpResponse( "first page")
     return response.HttpResponse(f"<html><head></head><body><span>request:</span></br>{request}</body></html>")
