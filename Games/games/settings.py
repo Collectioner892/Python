fdf64048de473a83fd253cd8baf3de5291370ce7
sdenisenko <sdenisen@ymail.com>
2021-08-26 17:08:14 +0300

    resolve: update configs to support heroku service;

--- Games/games/settings.py
+++ Games/games/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,3 +131,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
 ]
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
